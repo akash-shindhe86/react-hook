2631e0a2bd62ced0835723abbdfa932589cdf883
ashin19 <ashin19@safeway.com>
2025-02-12 12:17:09 -0800

    Add Jest setup and update tsconfig for custom matchers

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,6 @@
-const nextJest = require("next/jest");
-const createJestConfig = nextJest({
-    // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-    dir: './',
-});
-// Add any custom config to be passed to Jest
-/** @type {import('jest').Config} */
-const config = {
-    // Add more setup options before each test is run
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    testEnvironment: 'jest-environment-jsdom',
+// jest.config.js
+module.exports = {
     preset: 'ts-jest',
-    collectCoverage: true,
-    collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
-}
-
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(config);
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  };
