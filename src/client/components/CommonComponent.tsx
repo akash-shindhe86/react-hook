1c65ae7b25043d92a73b83f7ba0c2bee44b53d15
ashin19 <ashin19@safeway.com>
2025-02-13 14:29:50 -0800

    fixed.

--- src/client/components/CommonComponent.tsx
+++ src/client/components/CommonComponent.tsx
@@ -6,11 +6,10 @@ const selectors = {
     linkBlockContent: '#linkblock-placeholder',
     linkBlockPlaceHolder: '#linkblock-content',
     linkBlockStaticClass: 'linkblock-container--static',
-}
+};
 
 const moveContentToPlaceHolder = () => {
     const contentBlock = document.querySelector(selectors.linkBlockContent);
-
     const mktContentRepositioned = document.querySelector(selectors.linkBlockPlaceHolder);
 
     if (contentBlock && mktContentRepositioned) {
@@ -19,34 +18,32 @@ const moveContentToPlaceHolder = () => {
             contentBlock.insertBefore(mktContentRepositioned, contentBlock.nextSibling);
         }
     }
-}
+};
+
 const addScriptToDom = (className: string, src: string, onLoad?: any, onError?: any) => {
-    // Get all elements with the specified class
-    const elements = document.getElementsByClassName(className)
+    const elements = document.getElementsByClassName(className);
 
-    // Create a script tag
-    const script = document.createElement('script');
-    script.src = src;
-    script.async = true;
-    script.onload = onLoad;
-    script.onerror = onError;
+    if (elements.length > 0) {
+        const script = document.createElement('script');
+        script.src = src;
+        script.async = true;
+        script.onload = onLoad;
+        script.onerror = onError;
 
-    // Append the script tag to the first element
-    elements[0].appendChild(script);
+        elements[0].appendChild(script);
+    }
+};
 
-}
-const CommonComponent = ({configObj, config}: {configObj?: any, config? :any}) => {
-
+const CommonComponent = ({ configObj, config }: { configObj?: any, config?: any }) => {
     useEffect(() => {
         if (configObj?.pageType === 'pdp') {
             moveContentToPlaceHolder();
-            // @ts-ignore
             addScriptToDom('App-footer', 'https://pagead2.googlesyndication.com/tag/js/gpt.js', () => gamScriptLoaded(true), () => gamScriptLoaded(false));
         }
-        // script to load blue triangle
         addScriptToDom('App-footer', config?.blueTriangleUrl);
     }, []);
-    return (<></>)
-}
+
+    return (<></>);
+};
 
 export default CommonComponent;
