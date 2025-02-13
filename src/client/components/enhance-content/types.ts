4c38402a932cea7f022e9171d87ecf14e46eb3ce
ashin19 <ashin19@safeway.com>
2025-02-13 14:12:15 -0800

    added client/enchance

---
+++ src/client/components/enhance-content/types.ts
@@ -0,0 +1,13 @@
+
+
+export interface Items {
+    contentContainerSelector: string;
+    contentDetailSelector: string;
+    dNoneClass: string;
+    salsifyContentElemSelector: string;
+}
+
+export interface EnhanceContentProps {
+    config: any;
+    bpn: string;
+}
